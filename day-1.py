7034eae15be2780c5ac49f6b19ac3d52357a06ed
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-10-23 19:59:23 +0100

    Create day-1.py

---
+++ day-1.py
@@ -0,0 +1,9 @@
+print("""
+23 October, 2022
+My name is Johnpaul Chigozie, I just signed up for 100days of Python challenge!
+I will make sure to spend some time codding everyday.
+I will be using replit an amazing online code IDE so i can code using my phone 
+wherever i happen to be.
+I feel excited .....
+You can follow my progress here -->  https://replit.com/@ThePURITAN
+""")
