2d564515fdc64bfa802f4298aea46c83e031ddf4
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-10-24 03:52:30 +0100

    Create day2_100_days.py

---
+++ day2_100_days.py
@@ -0,0 +1,12 @@
+print('Getting to know You!')
+yourName = input("What's your name?:")
+favFood = input("What's your Favorite Food?:")
+favMusic = input("What's your favorite music?:")
+yourAddress = input("Where do you live?:")
+print()
+
+print("You are: ", yourName)
+print("You are probably hungry for: ", favFood)
+print("You are definitely getting your groove on to ", favMusic)
+print("You live in ", yourAddress)
+print("Have a great day!")
