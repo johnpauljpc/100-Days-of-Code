57e751e463391423c296d845d4f399373442c204
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-10-30 23:59:16 +0100

    Update day8.py

--- day8.py
+++ day8.py
@@ -1,1 +1,12 @@
 
+exit = ""
+while(exit !='yes'):
+  animal = input("What animal do you want?:  ")
+  if(animal.lower() == "cow"):
+    print("A cow goes moo.")
+  elif(animal.lower() == "dog"):
+    print("dog barks")
+  exit = input("Do You want to exit?: ")
+  
+  
+  
