088ca2723984ffd4892e22e9593e3c1b13316ca0
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-12-07 07:03:38 +0100

    Create day18.py

---
+++ day18.py
@@ -0,0 +1,9 @@
+print("**** Adds comma at the end of each character ****")
+for i in range(0, 20):
+  print(i, end =", ")
+print(" \n \n **** Adds a Tab space after each character ****  \n")
+for i in range(0, 20):
+  print(i, end="\t")
+print("\n **** Adds a slant space after each character ***")
+for i in range(0, 20):
+  print(i, end='\v')
