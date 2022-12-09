f584ce554724a5b9907f3b5c30534ce07d547727
Gabriel Johnpaul Chigozie <64210565+johnpauljpc@users.noreply.github.com>
2022-12-09 09:58:02 +0100

    f-strings

---
+++ day21.py
@@ -0,0 +1,6 @@
+name = "John"
+age = "128"
+
+# print("This is {n} using {p} pronouns and is age {a}".format(n=name, p=pronouns, a=age))
+response = f"My name is {name}, I'm {age}yrs old"
+print(response)
